76ea2940c58c4b1276472610768a730cd1650324
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-02 15:28:19 +0530

    Overmind

--- src/overmind-notes-hook.js
+++ src/overmind-notes-hook.js
@@ -1,36 +1,7 @@
 import { Overmind } from 'overmind'
 import { namespaced } from 'overmind/config'
 import { createHook } from 'overmind-react'
-import {
-  appendChildId,
-  createInitialNotesByIdState,
-  createNewNote,
-  ROOT_NOTE_ID,
-} from './models/note'
-
-const notes = {
-  state: {
-    byId: createInitialNotesByIdState(),
-    parentIds: {},
-    root: ({ byId }) => byId[ROOT_NOTE_ID],
-    rootChildren: ({ byId, root }) => root.childIds.map(cid => byId[cid]),
-  },
-  actions: {
-    addNewNote,
-  },
-  effects: {},
-}
-
-function addNewNote({
-  state: {
-    notes: { byId, parentIds, root },
-  },
-}) {
-  const n = createNewNote()
-  byId[n.id] = n
-  appendChildId(n.id, root)
-  parentIds[n.id] = root.id
-}
+import { notes } from './notes'
 
 const overmind = new Overmind(namespaced({ notes }), {
   name: 'Overmind Notes',
