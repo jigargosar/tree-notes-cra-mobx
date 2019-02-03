300d2a486dc16885b94cd779a843f859fc6dd4a8
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-03 13:00:07 +0530

    useHooks

--- src/state-hooks.js
+++ src/state-hooks.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import * as R from 'ramda'
 import validate from 'aproba'
+import { useSetState } from 'react-use'
 
 export function useLookup(initialList = [], options = {}) {
   validate('AO|A|', arguments)
 
   const { getId = R.prop('id') } = options
-  const [state, setState] = React.useState(
+  const [state, setState] = useSetState(
     initialList.reduce((acc, item) => {
       acc[getId(item)] = item
       return acc
     }, {}),
   )
 
-  const set = (k, v) => setState(R.mergeLeft({ [k]: v }))
+  const set = (k, v) => lookup.set(R.mergeLeft({ [k]: v }))
   const get = k => state[k]
   const has = k => R.isNil(get(k))
   const keys = () => Object.keys(state)
