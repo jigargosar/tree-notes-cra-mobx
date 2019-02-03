d829206c2cd7501448a9e2ebbc9bc79c8b2d46bf
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-03 17:43:58 +0530

    use mobx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react'
 import * as R from 'ramda'
+import { observer } from 'mobx-react-lite'
+import { createInitialNotesByIdState } from './models/note'
+import { autorun } from 'mobx'
+import { cache } from './utils'
 
-function App() {
+class NoteTree {
+  byId = createInitialNotesByIdState()
+  parentIds = {}
+  selectedId = null
+
+  constructor() {
+    autorun(() => {
+      cache('noteTree', R.pick(['byId', 'selectedId'])(this))
+    })
+  }
+}
+
+const nt = new NoteTree()
+
+const RootTree = observer(() => {
+  return <div className="">RT</div>
+})
+
+const App = observer(() => {
   return (
     <div className="w-80 center sans-serif">
       <div className="pv3 f4 ttu tracked">Tree Notes</div>
@@ -13,9 +35,10 @@ function App() {
           delete all
         </button>
       </div>
-      <div className="pv3">RT</div>
+      <div className="pv3">
+        <RootTree />
+      </div>
     </div>
   )
-}
-
+})
 export default App
