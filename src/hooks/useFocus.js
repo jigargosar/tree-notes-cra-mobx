75d1e09643c5b0e79b6862c7156ac6261d694900
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-04 22:00:18 +0530

    use mobx

--- src/hooks/useFocus.js
+++ src/hooks/useFocus.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import scrollIntoView from 'scroll-into-view-if-needed'
 
 export function useFocusRef(ref, shouldFocus, deps = null) {
   React.useLayoutEffect(() => {
     console.log(`shouldFocus`, shouldFocus)
     const el = ref.current
     if (el && shouldFocus) {
-      el.blur()
+      // if (el !== document.activeElement) {
+      //   el.focus()
+      // } else {
+      //   el.scrollIntoView({
+      //     behavior: 'auto',
+      //     block: 'center',
+      //     inline: 'nearest',
+      //   })
+      // }
+
+      scrollIntoView(el, {
+        behavior: 'smooth',
+        scrollMode: 'if-needed',
+        block: 'center',
+        inline: 'center',
+      })
       el.focus()
     }
   }, deps)
