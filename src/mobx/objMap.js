4d724e680c4632a3c9fc053d82f9ffb73691aeac
Jigar Gosar <jigar.gosar@gmail.com>
2019-02-04 14:24:50 +0530

    use mobx

--- src/mobx/objMap.js
+++ src/mobx/objMap.js
@@ -1,9 +1,16 @@
-import { extendObservable, isObservable, observable } from 'mobx'
+import { action, extendObservable, isObservable, observable } from 'mobx'
 
 function createObjMap(initial = {}, options) {
   if (!isObservable(initial)) {
     initial = observable.object(initial, null, options)
   }
 
-  return extendObservable(initial, {})
+  return extendObservable(
+    initial,
+    {
+      get: k => initial[k],
+      set: (k, v) => (initial[k] = v),
+    },
+    { get: action, set: action },
+  )
 }
